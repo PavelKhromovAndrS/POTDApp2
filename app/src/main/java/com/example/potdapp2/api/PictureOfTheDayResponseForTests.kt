8968edd296c4f2b1f37347b55afe92da983f8aa7
PavelKhromovAndrS <pavelkhromov1993@yandex.ru>
2023-03-06 14:26:28 +0300

    Added tests to my app 2

---
+++ app/src/main/java/com/example/potdapp2/api/PictureOfTheDayResponseForTests.kt
@@ -0,0 +1,31 @@
+package com.example.potdapp2.api
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+data class PictureOfTheDayResponseForTests(
+
+    @SerializedName("copyright")
+    val copyright: String,
+
+    @SerializedName("explanation")
+    val explanation: String,
+
+    @SerializedName("hdurl")
+    val hdurl: String,
+
+    @SerializedName("media_type")
+    val mediaType: String,
+
+    @SerializedName("service_version")
+    val serviceVersion: String,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("url")
+    val url: String
+
+)
+
+
---
+++ app/src/main/java/com/example/potdapp2/api/PictureOfTheDayResponseFunctionForTests.kt
@@ -0,0 +1,7 @@
+package com.example.potdapp2.api
+
+class PictureOfTheDayResponseFunctionForTests {
+companion object PictureOfTheDay{
+    val pictureOfTheDay = PictureOfTheDayResponseForTests("Nasa","abrakadabra","nasa.com","HDUrl","1","Moon","moon.php")
+
+}}
